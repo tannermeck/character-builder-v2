3a0a9ceee00c0890d5c7df8c2afa4d377f270def
TANNER MECK <steve.meck246@gmail.com>
2021-11-29 17:58:45 -0800

    redo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import './App.css'
+import React, { useState } from 'react'
+import Body from './presentation/Body'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from character-builder</h1>
+      <Body />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/presentation/Body.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Body(){
+    return <h1>Hello from body</h1>
+}
